b651873c2d89370ec6c69b6afcd91974f0beb8d2
Ravi2712 <patel.ravi2712@gmail.com>
2017-04-02 13:56:05 -0700

    Initial Commit

---
+++ KataBowling-Challenge/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KataBowling-Challenge
+//
+//  Created by Ravi Patel on 4/2/17.
+//  Copyright © 2017 Jay Patel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
